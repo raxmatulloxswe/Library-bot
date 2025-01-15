2939d731ebd731050d4b20ee0b51b00b0d4c0396
Raxmatullox <irahmatjon06@gmail.com>
2025-01-15 16:24:12 +0500

    update runnbot.py, added line 40

--- apps/bot/management/commands/runbot.py
+++ apps/bot/management/commands/runbot.py
@@ -10,8 +10,10 @@ from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.redis import RedisStorage
 from aiogram.utils.i18n import I18n, SimpleI18nMiddleware, ConstI18nMiddleware
 
-from apps.bot.middlewares.i18n_middleware import CustomI18nMiddleware
 from apps.bot.handlers import setup_handlers
+from apps.bot.handlers.commands import set_commands
+from apps.bot.middlewares.i18n_middleware import CustomI18nMiddleware
+# from apps.bot.handlers import setup_handlers
 from apps.bot.middlewares import setup_middlewares
 from apps.bot.config.config import BOT_TOKEN, REDIS_URL
 
@@ -29,13 +31,13 @@ class Command(BaseCommand):
 
         bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
         dp = Dispatcher(storage=RedisStorage(redis=redis))
-
         i18n = I18n(path="locales", default_locale="en", domain="messages")
         i18n_middleware = CustomI18nMiddleware(i18n=i18n)
         
         i18n_middleware.setup(dp)
 
         setup_handlers(dp)
+        await set_commands(bot)
         setup_middlewares(dp)
 
         await bot.delete_webhook(drop_pending_updates=True)
