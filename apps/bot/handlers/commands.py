3cce6650b04e6a25f50d8c80c8a2588f89ba3acb
Raxmatullox <irahmatjon06@gmail.com>
2025-01-15 16:23:24 +0500

    update commands.py, added set_commands func

--- apps/bot/handlers/commands.py
+++ apps/bot/handlers/commands.py
@@ -1,6 +1,7 @@
-from aiogram import Router, types
-from aiogram.filters import CommandStart
+from aiogram import Router, types, Bot
+from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
+from aiogram.types import BotCommand
 from aiogram.utils.i18n import gettext as _
 from aiogram.utils.i18n import lazy_gettext as __
 
@@ -12,3 +13,17 @@ router = Router()
 @router.message(CommandStart())
 async def start_command(message: types.Message, state: FSMContext):
     await message.answer(_("Botga xush kelibsiz!"), reply_markup=reply_main_menu())
+
+
+@router.message(Command('check_state'))
+async def check_state(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    await message.answer(f"State: {current_state}")
+
+
+async def set_commands(bot: Bot):
+    commands = [
+        BotCommand(command="start", description="Botni ishga tushirish!"),
+        BotCommand(command="check_state", description="Qaysi stateda ekanligini ko'rsatadi!"),
+    ]
+    await bot.set_my_commands(commands)
