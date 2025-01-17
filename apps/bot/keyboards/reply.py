b6788989b77348013e90b66efe27cc3f0a9fcd42
Raxmatullox <irahmatjon06@gmail.com>
2025-01-17 18:41:02 +0500

    Add: Added reply_check_result_or_add_new_result button

--- apps/bot/keyboards/reply.py
+++ apps/bot/keyboards/reply.py
@@ -23,3 +23,24 @@ def reply_settings_menu():
     reply_keyboard.adjust(2)
 
     return reply_keyboard.as_markup(resize_keyboard = True)
+
+
+def reply_check_result_or_add_new_result():
+    reply_keyboard = ReplyKeyboardBuilder()
+
+    reply_keyboard.button(text=_("Ha, eslab qolinsin✅"))
+    reply_keyboard.button(text=_("Yo'q ❌"))
+    reply_keyboard.button(text=_("Yangi natija qo'shish ➕"))
+
+    reply_keyboard.adjust(1, 2)
+    return reply_keyboard.as_markup(resize_keyboard=True)
+
+
+# def reply_add_new_result_or_back_main_menu():
+#     reply_keyboard = ReplyKeyboardBuilder()
+#
+#     reply_keyboard.button(text=_("Yangi natija qo'shish 📨"))
+#     reply_keyboard.button(text=_("Bosh menyuga qaytish 🔙"))
+#
+#     reply_keyboard.adjust(1)
+#     return reply_keyboard.as_markup(resize_keyboard=True)
