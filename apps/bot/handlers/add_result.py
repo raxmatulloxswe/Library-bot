d7ee06062479db8ffcce5b8f6296a9679ab48edb
Raxmatullox <irahmatjon06@gmail.com>
2025-01-17 18:40:25 +0500

    Add: Added result_confirm_or_add_new_result() function

--- apps/bot/handlers/add_result.py
+++ apps/bot/handlers/add_result.py
@@ -6,6 +6,7 @@ from aiogram.utils.i18n import lazy_gettext as __
 from aiogram.fsm.context import FSMContext
 from asgiref.sync import sync_to_async
 
+from apps.bot.keyboards.reply import reply_main_menu, reply_check_result_or_add_new_result
 from apps.bot.models import Book, Results
 from apps.bot.utils.states import AddResultStatesGroup
 from apps.bot.keyboards.inline import * # noqa
@@ -50,14 +51,12 @@ async def add_result_for_book(message: types.Message, state: FSMContext):
     try:
         num = int(message.text)
         if 10 <= num <= 200:
-            state_data = await state.get_data()
-            book_name = state_data['book_name']
-
-            await message.answer(f"Siz o'qigan {num} bet eslab qolindi!")
-            await Results.objects.acreate(
-                user_id=message.from_user.id,
-                book_name=book_name,
-                read_pages_count=num)
+            await message.answer(f"Siz o'qigan <b>{num}</b> bet eslab qolinsinmi?",  reply_markup=reply_check_result_or_add_new_result())
+            await state.update_data(
+                {
+                    "user_read_pages_count": num
+                 }
+            )
             await state.set_state(AddResultStatesGroup.result_confirm)
         else:
             await message.answer('10 va 200 oralig\'ida o\'qishingiz kerak va shu oraliqda son kiriting!')
@@ -65,3 +64,29 @@ async def add_result_for_book(message: types.Message, state: FSMContext):
         print(e)
         await message.answer("Boshidan urinib ko'ring!")
 
+
+@router.message(AddResultStatesGroup.result_confirm)
+async def result_confirm_or_add_new_result(message: types.Message, state: FSMContext):
+    print(message.text)
+    if message.text == _("Ha, eslab qolinsin✅"):
+        state_data = await state.get_data()
+        book_name = state_data['book_name']
+        user_read_pages_count = state_data['user_read_pages_count']
+
+        await message.answer(f"<b>{book_name}</b> kitobining siz o'qigan {user_read_pages_count} beti eslab qolindi!", reply_markup=reply_main_menu())
+        await Results.objects.acreate(
+            user_id=message.from_user.id,
+            book_name=book_name,
+            read_pages_count=user_read_pages_count
+        )
+        await state.clear()
+    elif message.text == _("Yo'q ❌"):
+        await state.set_state(AddResultStatesGroup.go_to_book)
+        await prompt_user_to_choose_another_book(message, state)
+    elif message.text == _("Yangi natija qo'shish ➕"):
+        await state.set_state(AddResultStatesGroup.go_to_book)
+        await prompt_user_to_choose_another_book(message, state)
+
+async def prompt_user_to_choose_another_book(message: types.Message, state: FSMContext):
+    books = await sync_to_async(list)(Book.objects.all().only('id', 'name'))
+    await message.answer('Iltimos, o\'qigan kitobingizni tanlang:', reply_markup=inline_books_pagination(books))
